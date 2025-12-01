52534030573924abd7ae60539d3a9b15ea60ee92
oonohonoka <siw25020008@class.siw.ac.jp>
2025-12-01 23:32:54 +0900

    Initial commit

---
+++ lesson/lessonApp.swift
@@ -0,0 +1,17 @@
+//
+//  lessonApp.swift
+//  lesson
+//
+//  Created by 大野帆乃楓 on 2025/12/01.
+//
+
+import SwiftUI
+
+@main
+struct lessonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
